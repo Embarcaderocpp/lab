b69bed4bf1e75d22fe3053fbc5f796dd34ad8fc4
sha-va <Embarcaderocpp@gmail.com>
2025-10-13 11:21:06 +0300

    Update main.py
    fix

--- 4_2/main.py
+++ 4_2/main.py
@@ -7,7 +7,7 @@ if x_1 < x_2 and x_1 < x_3 and x_1 < x_4:
     result = f"min x = {x_1}"
 else:
     if x_2 < x_3 and x_2 < x_4 and x_2 < x_1:
-        result = f"min x = {x_3}"
+        result = f"min x = {x_2}"
     else:
         if x_3 < x_1 and x_3 < x_2 and x_3 < x_4:
             result = f"min x = {x_3}"
