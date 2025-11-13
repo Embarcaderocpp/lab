01dee024908d2490a4cf9d316bfecb1042ca4447
sha-va <Embarcaderocpp@gmail.com>
2025-11-13 12:02:19 +0300

    Update main.py

--- 4_1/main.py
+++ 4_1/main.py
@@ -1,16 +1,14 @@
 import math
-
-def f(x):
+   
+x  =  float(input("x:"))
+if x > 1:
+    print('x=',x,' "При 1<x функция  не определена.")
+else:          
     if x < -2:
         result = 3*x+math.sqrt(abs(x+1))
-    if -2 <= x - 1.5:
-        result = (math.sin(x) + math.cos(x)) / (2*x + 1)
-    if -1 < x <= 1:
-        result = math.e**(-3*x+2*x-1)
-    if x > 1:
-        result = "При 1<x функция  не определена."
-    return result
-    
-x  =  float(input("x:"))
-result = f(x)
-print(f"результат: {result}")
+    else:    
+        if -2 <= x - 1.5:
+            result = (math.sin(x) + math.cos(x)) / (2*x + 1)
+        else:
+            result = math.e**(-3*x+2*x-1)
+    print('x=', x, f"результат: {result}")
